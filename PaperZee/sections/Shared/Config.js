a4b1abfe25ce2a18474108b795ec8edb10d727e3
Sam Hindess <mail@sohcah.dev>
2020-06-08 22:41:53 +0100

    Change Config

---
+++ PaperZee/sections/Shared/Config.js
@@ -0,0 +1,16 @@
+import {Platform} from 'react-native';
+export default Platform.OS=="web"?(
+  __DEV__?{
+    useProxy: false,
+    redirect_uri: "http://localhost:19006/auth"
+  }:{
+    useProxy: false,
+    redirect_uri: "https://cuppazee.app/auth"
+  }
+):((__DEV__||Platform.OS=="android"||(Platform.OS=="ios"&&parseInt(Platform.Version, 10)<12))?{
+  useProxy: true,
+  redirect_uri: 'https://auth.expo.io/@sohcah/PaperZee'
+}:{
+  useProxy: false,
+  redirect_uri: 'cuppazee://auth'
+})
